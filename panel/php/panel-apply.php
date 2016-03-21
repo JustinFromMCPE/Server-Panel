55a31107974ea9be15879c60e4a0914a93f96788
Rocket-Scientist2 <colecrouter@hotmail.com>
2016-03-21 10:33:08 -0600

    Update panel-apply.php

--- panel/php/panel-apply.php
+++ panel/php/panel-apply.php
@@ -1,5 +1,5 @@
 <?php
 $settings = "../options.txt";
-$new = "name=".$_POST['name']."\nusername=".md5($_POST['username'])."\npassword=".md5($_POST['password'])."\nwrapper-port=".$_POST['port']."\nserver-jar=".$_POST['jar']."\nserver-port=".$_POST['server-port']."\nram=".$_POST['ram']."\nmap=".$_POST["map"];
+$new = "name=".$_POST['name']."\nusername=".password_verify($_POST['username'], PASSWORD_DEFAULT)."\npassword=".password_hash($_POST['password'], PASSWORD_DEFAULT)."\nwrapper-port=".$_POST['port']."\nserver-jar=".$_POST['jar']."\nserver-port=".$_POST['server-port']."\nram=".$_POST['ram']."\nmap=".$_POST["map"];
 file_put_contents($settings, $new);
 ?>
